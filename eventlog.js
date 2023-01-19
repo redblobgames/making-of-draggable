0168d0248f38fec008732dba887d2a06743815d3
Amit Patel <redblobgames@gmail.com>
2023-01-19 11:46:27 -0800

    Feat: more event tests, many more drag tests

--- eventlog.js
+++ eventlog.js
@@ -21,54 +21,44 @@ function convertPixelToSvgCoord(event) {
 
 
 
-let pre = document.querySelector("main pre");
+let log = document.querySelector("#log");
 let svg = document.querySelector("main figure svg");
 let draggable = svg.querySelector("g");
 
-let handlers = {
-    click(event) {},
-    dblclick(event) {},
-    auxclick(event) {},
-    contextmenu(event) {},
-    mousedown(event) {},
-    mouseup(event) {},
-    mouseenter(event) {},
-    mouseleave(event) {},
-    mouseover(event) {},
-    mouseout(event) {},
-    pointerdown(event) {},
-    pointerup(event) {},
-    pointercancel(event) {},
-    pointerenter(event) {},
-    pointerleave(event) {},
-    pointerover(event) {},
-    pointerout(event) {},
-    //pointermove(event) {},
-    //pointerrawupdate(event) {},
-    touchstart(event) {},
-    touchend(event) {},
-};
+let eventNames = `
+click dblclick auxclick contextmenu
+mousedown mouseup mouseenter mouseleave mouseover mouseout
+pointerdown pointerup pointercancel pointerenter pointerleave pointerover pointerout
+touchstart touchend touchcancel
+webkitmouseforcewillbegin webkitmouseforcedown webkitmouseforceup`.trim().split(/\s/);
 
+let moveEventNames = `
+mousemove
+pointermove
+touchmove
+webkitmouseforcechanged`.trim().split(/\s/);
+
+// First test on the page shows the event log except for move events
 let outputQueue = [];
 function showEvents() {
     if (outputQueue.length > 0) {
-        pre.textContent = `${pre.textContent}\n${new Date().toLocaleTimeString()} ${outputQueue.join(' ')}`;
+        log.textContent = `${log.textContent}\n${new Date().toLocaleTimeString()} ${outputQueue.join(' ')}`;
         outputQueue = [];
     }
     requestAnimationFrame(showEvents);
 }
 showEvents();
 
-for (let type of Object.keys(handlers)) {
+for (let type of eventNames) {
     draggable.addEventListener(type, (event) => {
         let output = event.type;
         if (event.pointerId !== undefined) output += "." + event.pointerId;
         if (event.target.tagName === 'text') output += ".T";
         if (event.target.tagName === 'circle') output += ".C";
         outputQueue.push(output);
-        handlers[type](event);
     });
 }
+
 // draggable.addEventListener('pointermove', (event) => {
 //     let p = convertPixelToSvgCoord(event);
 //     draggable.style.transform = `translate(${p.x}px, ${p.y}px)`;
@@ -77,3 +67,48 @@ for (let type of Object.keys(handlers)) {
 function clearPre() {
     pre.textContent = "";
 }
+
+
+// Second test on the page shows the current pointerdata only
+let testBox = document.querySelector("#testbox");
+let pointerData = document.querySelector("#pointerdata");
+let touchData = document.querySelector("#touchdata");
+let forceData = document.querySelector("#forcedata");
+
+for (let type of moveEventNames.concat(eventNames)) {
+    testBox.addEventListener(type, (event) => {
+        if (event.pointerType)
+            pointerData.textContent = `PointerEvent
+event:              ${event.type}
+pointerType:        ${event.pointerType}
+isPrimary:          ${event.isPrimary}
+pointerId:          ${event.pointerId}
+width:              ${event.width}
+height:             ${event.height}
+pressure:           ${event.pressure}
+webkitForce:        ${event.webkitForce}
+tangentialPressure: ${event.tangentialPressure}
+tiltX:              ${event.tiltX}
+tiltY:              ${event.tiltY}
+twist:              ${event.twist}`;
+
+        if (event.webkitForce)
+            forceData.textContent = `${event.type}  .webkitForce: ${event.webkitForce}`;
+        
+        if (event.changedTouches) {
+            let touch = event.changedTouches[0];
+            touchData.textContent = `TouchEvent.Touch
+identifier:         ${touch.identifier}
+touchType:          ${touch.touchType}
+radiusX:            ${touch.radiusX}
+radiusY:            ${touch.radiusY}
+rotationAngle:      ${touch.rotationAngle}
+altitudeAngle:      ${touch.altitudeAngle}
+azimuthAngle:       ${touch.azimuthAngle}
+force:              ${touch.force}`;
+        }
+
+        // There's also event.webkitForce on MouseEvent
+    });
+}
+
---
+++ tests.js
@@ -0,0 +1,270 @@
+/*
+ * From https://www.redblobgames.com/x/2251-draggable/
+ * Copyright 2022 Red Blob Games <redblobgames@gmail.com>
+ * @license Apache-2.0 <https://www.apache.org/licenses/LICENSE-2.0.html>
+ */
+'use strict';
+
+console.info("I'm happy to answer questions about the code; email me at redblobgames@gmail.com");
+
+
+function clamp(x, lo, hi) { return x < lo ? lo : x > hi ? hi : x; }
+
+/** Convert from event coordinate space (on the page) to SVG coordinate
+ * space (within the svg, honoring responsive resizing, width/height,
+ * and viewBox */
+function convertPixelToSvgCoord(event) {
+    const svg = event.currentTarget.ownerSVGElement;
+    let p = svg.createSVGPoint();
+    p.x = event.clientX;
+    p.y = event.clientY;
+    p = p.matrixTransform(svg.getScreenCTM().inverse());
+    return p;
+}
+
+/** Convert from event coordinate space (on the page) to Canvas coordinate
+ * space (assuming there are no transforms on the canvas) */
+function convertPixelToCanvasCoord(event) {
+    const canvas = event.currentTarget;
+    const bounds = canvas.getBoundingClientRect();
+    return {
+        x: (event.x - bounds.left) / bounds.width * canvas.width,
+        y: (event.y - bounds.top) / bounds.height * canvas.height,
+    };
+}
+
+
+// config object should have el:String, draw:()=>(), and event handlers
+function Diagram(figure, config) {
+    let pos = {x: 0, y: 0};
+    let state = {
+        ...config,
+        figure,
+        el: figure.querySelector(config.el),
+        get pos() { return pos; },
+        set pos({x, y}) {
+            pos.x = clamp(x, this.left, this.right);
+            pos.y = clamp(y, this.top, this.bottom);
+            config.draw.call(state);
+        },
+        dragging: null, // either null or value set by handler
+    };
+    
+    for (let prop of Object.keys(config)) {
+        if (prop.startsWith("on")) state.el[prop] = (event) => state[prop](event);
+    }
+    
+    function reset() { state.pos = {x: 0, y: 0}; }
+    let button = figure.querySelector("button");
+    if (button) button.addEventListener('click', reset);
+    reset();
+}
+
+
+// TODO: the six cases share some code in common, which should end up in the draggable.v3 library
+// 1. this.dragging state
+//     * should be in screen coordinates not svg/canvas coordinates
+//        because svg/canvas coordinates are not known by the library
+// 2. add/remove class (specified by config)
+// 3. add/remove user-select:none
+// 4. preventDefault on two events
+// 5. pointer capture
+//
+// but this doesn't handle the canvas case, so should I even bother? maybe I should make an
+// svg-only library and deal with canvas separately?
+
+const svgDragHandlersCommon = {
+    left: -300, right: 300, top: -20, bottom: 20,
+    onpointerdown(event) {
+        let initialPos = convertPixelToSvgCoord(event);
+        this.dragging = {dx: this.pos.x - initialPos.x, dy: this.pos.y - initialPos.y};
+        this.el.classList.add("dragging");
+        // if you use event.target, the text will get the event and it
+        // will bubble up, but the text can get selected when dragging
+        event.currentTarget.setPointerCapture(event.pointerId);
+        // and we want to disable text selection only while dragging
+        // so the .dragging class will set CSS user-select:none
+        this.onpointermove(event);
+    },
+    onpointerup(event) {
+        this.dragging = null;
+        this.el.classList.remove("dragging");
+    },
+    onpointercancel(event) {
+        this.onpointerup(event);
+    },
+    onpointermove(event) {
+        if (!this.dragging) return;
+        let {x, y} = convertPixelToSvgCoord(event);
+        // TODO: need to test whether the offset tracking gets messed up by screen rotation or diagram resizing (e.g. size set in vh/vw)
+        this.pos = {x: x + this.dragging.dx, y: y + this.dragging.dy};
+    },
+    ondragstart(event) {
+        // Prevent dragging text
+        event.preventDefault();
+    },
+    ontouchstart(event) {
+        // Prevent scrolling on mobile; make sure this is an active event handler https://github.com/WICG/EventListenerOptions/blob/gh-pages/explainer.md
+        // TODO: it defaults to true?? https://developer.mozilla.org/en-US/docs/Web/API/EventTarget/addEventListener#improving_scrolling_performance_with_passive_listeners
+        // TODO: I don't think we can use touch-action:none here because that only applies to the ancestor. But maybe we can conditionally apply that depending on whether dragging is true? But that'd mean we have to go mess with an ancestor element, ugh. https://developer.mozilla.org/en-US/docs/Web/CSS/touch-action
+        event.preventDefault();
+    },
+};
+
+const divDragHandlersCommon = {
+    left: 0, top: 0, right: 1000, bottom: 75,
+    onpointerdown(event) {
+        this.dragging = {dx: this.pos.x - event.clientX, dy: this.pos.y - event.clientY};
+        event.currentTarget.setPointerCapture(event.pointerId);
+        event.currentTarget.classList.add("dragging");
+        this.onpointermove(event);
+    },
+    onpointerup(event) {
+        this.dragging = null;
+        event.currentTarget.classList.remove("dragging");
+    },
+    onpointercancel(event) {
+        this.onpointerup(event);
+    },
+    onpointermove(event) {
+        if (!this.dragging) return;
+        // HACK: adjust the right/bottom bounds based on the current size, especially needed when resizing the browser/font
+        let outer = this.el.parentElement.getBoundingClientRect();
+        let inner = this.el.getBoundingClientRect();
+        this.right = outer.width - inner.width;
+        this.bottom = outer.height - inner.height;
+        // Now we can set the position, and the Diagram will use the updated bounds
+        this.pos = {x: event.clientX + this.dragging.dx, y: event.clientY + this.dragging.dy};
+    },
+    ondragstart(event) {
+        // Prevent dragging text
+        event.preventDefault();
+    },
+    ontouchstart(event) {
+        // Prevent scrolling on mobile
+        event.preventDefault();
+    },
+};
+
+const diagrams = [
+    {
+        el: "svg circle",
+        draw() {
+            this.el.setAttribute('transform', `translate(${this.pos.x}, ${this.pos.y})`);
+        },
+        ...svgDragHandlersCommon
+    },
+    {
+        el: "svg g",
+        draw() {
+            this.el.setAttribute('transform', `translate(${this.pos.x}, ${this.pos.y})`);
+        },
+        ...svgDragHandlersCommon
+    },
+    {
+        el: "canvas",
+        left: 0, top: 0, right: 660, bottom: 100,
+        draw() {
+            if (!this.dragging) return;
+            let ctx = this.el.getContext('2d');
+            ctx.fillStyle = "hsl(0 50% 50%)";
+            ctx.fillRect(this.pos.x-1, this.pos.y-1, 3, 3);
+        },
+        onpointerdown(event) {
+            this.dragging = true;
+            event.currentTarget.setPointerCapture(event.pointerId);
+            this.onpointermove(event);
+        },
+        onpointerup(event) {
+            this.dragging = null;
+        },
+        onpointercancel(event) {
+            this.onpointerup(event);
+        },
+        onpointermove(event) {
+            if (!this.dragging) return;
+            let {x, y} = convertPixelToCanvasCoord(event);
+            this.pos = {x, y};
+        },
+        ondragstart(event) {
+            // Prevent dragging text
+            event.preventDefault();
+        },
+        ontouchstart(event) {
+            // Prevent scrolling on mobile, anywhere in the canvas
+            event.preventDefault();
+        },
+    },
+    {
+        el: "canvas",
+        left: 46, top: 46, right: 1000-46, bottom: 150-46,
+        radius: 45,
+        draw() {
+            let ctx = this.el.getContext('2d');
+            ctx.clearRect(0, 0, this.el.width, this.el.height);
+            ctx.fillStyle = this.dragging? "hsl(200 50% 50%)" : "hsl(0 50% 50%)";
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            ctx.arc(this.pos.x, this.pos.y, this.radius, 0, 2*Math.PI);
+            ctx.fill();
+            ctx.stroke();
+            ctx.fillStyle = "white";
+            ctx.textAlign = "center";
+            ctx.font = "24px serif";
+            ctx.fillText("Drag me", this.pos.x, this.pos.y + 5);
+        },
+        isOverDragHandle(pos) {
+            return Math.hypot(pos.x - this.pos.x, pos.y - this.pos.y) <= this.radius;
+        },
+        onpointerdown(event) {
+            let initialPos = convertPixelToCanvasCoord(event);
+            if (!this.isOverDragHandle(initialPos)) return;
+            this.dragging = {dx: this.pos.x - initialPos.x, dy: this.pos.y - initialPos.y};
+            event.currentTarget.setPointerCapture(event.pointerId);
+            this.onpointermove(event);
+        },
+        onpointerup(event) {
+            this.dragging = null;
+            this.onpointermove(event); // to set cursor
+            this.draw();
+        },
+        onpointercancel(event) {
+            this.onpointerup(event);
+        },
+        onpointermove(event) {
+            let {x, y} = convertPixelToCanvasCoord(event);
+            this.el.style.cursor = !this.isOverDragHandle({x, y})? '' : this.dragging? 'grabbing' : 'grab';
+            if (!this.dragging) return;
+            this.pos = {x: x + this.dragging.dx, y: y + this.dragging.dy};
+        },
+        ondragstart(event) {
+            // Prevent dragging text
+            event.preventDefault();
+        },
+        ontouchstart(event) {
+            // Prevent scrolling on mobile, but only if over the drag handle
+            let pos = convertPixelToCanvasCoord(event);
+            if (this.dragging || this.isOverDragHandle(pos)) event.preventDefault();
+        },
+    },
+    {
+        el: "div.draggable",
+        draw() {
+            this.el.style.left = this.pos.x + 'px';
+            this.el.style.top = this.pos.y + 'px';
+        },
+        ...divDragHandlersCommon
+    },
+    {
+        el: "div.draggable",
+        draw() {
+            this.el.style.transform = `translate(${this.pos.x}px, ${this.pos.y}px)`;
+        },
+        ...divDragHandlersCommon
+    },
+];
+
+diagrams.forEach((diagram, index) => {
+    let figure = document.querySelectorAll("figure")[index];
+    Diagram(figure, diagram);
+});
